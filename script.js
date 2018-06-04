83bb2a23774d0b589c0da5a0a8d501c00413f39e
kirikrush <perdens@gmail.com>
2018-06-04 14:41:22 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
 
 $(document).ready(() => {
-  let list = ("#i0>span")
+  let list = ("#i0>span");
+  let sliderButton = ("#s0");
 
 
-  $(".list__item__service").on('click', event => {
+  $(".list__item__service>span").on('click', event => {
 
     $(list).css("outline", "none");
     list = event.target;
@@ -14,17 +15,40 @@ $(document).ready(() => {
     let itemHeight = +$(".slider__item").css("height").split("px")[0];
     let itemMargin = +$(".slider__item").css("margin-bottom").split("px")[0];
     let letItemMarginBot;
-  let shift = Math.round(itemHeight + itemMargin) * multipler;
-  console.log(multipler, bgPosition, shift);
-  $(".presentation").css({
-    "background-position-y": bgPosition
+    let shift = Math.round(itemHeight + itemMargin) * multipler;
+    console.log(multipler, bgPosition, shift);
+    $(".presentation").css({
+      "background-position-y": bgPosition
+    })
+    $(".slider__item").css({
+      "top": "-" + String(shift) + "px"
+    })
   })
-  $(".slider__item").css({
-    "top": "-" + String(shift) + "px"
+
+  $(".slider__control").on('click', event => {
+    console.log('okay')
+    $(sliderButton).css({
+      "background-position": "-300px,0",
+      "color":"black"
+    })
+
+    sliderButton = event.target;
+
+    $(sliderButton).css({
+      "background-position": "0,0",
+      "color":"white"
+    })
+    let multipler = +$(event.currentTarget).attr("id")[1];
+    let bgPosition = (1000 * multipler) + "px";
+    let itemHeight = +$(".slider__item__horizontal").css("width").split("px")[0];
+    let itemMargin = +$(".slider__item__horizontal").css("margin-right").split("px")[0];
+    let pad =  +$(document).width() * 0.1;
+    let shift = Math.round(itemHeight + itemMargin )* multipler;
+    console.log(multipler, itemHeight,itemMargin,pad, shift);
+    $(".slider__item__horizontal").css({
+      "left": "-" + String(shift) + "px"
+    })
   })
-})
-
-
 
 
 
