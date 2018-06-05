d9016e454736a161cf170fd487b0453f98a3790f
kirikrush <perdens@gmail.com>
2018-06-05 11:40:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -29,28 +29,59 @@ $(document).ready(() => {
     console.log('okay')
     $(sliderButton).css({
       "background-position": "-300px,0",
-      "color":"black"
+      "color": "black"
     })
 
     sliderButton = event.target;
 
     $(sliderButton).css({
       "background-position": "0,0",
-      "color":"white"
+      "color": "white"
     })
     let multipler = +$(event.currentTarget).attr("id")[1];
     let bgPosition = (1000 * multipler) + "px";
     let itemHeight = +$(".slider__item__horizontal").css("width").split("px")[0];
     let itemMargin = +$(".slider__item__horizontal").css("margin-right").split("px")[0];
-    let pad =  +$(document).width() * 0.1;
-    let shift = Math.round(itemHeight + itemMargin )* multipler;
-    console.log(multipler, itemHeight,itemMargin,pad, shift);
+    let pad = +$(document).width() * 0.1;
+    let shift = Math.round(itemHeight + itemMargin) * multipler;
+    console.log(multipler, itemHeight, itemMargin, pad, shift);
     $(".slider__item__horizontal").css({
       "left": "-" + String(shift) + "px"
     })
   })
+  let slider = 0;
 
 
+  $("#prev").on('click', event => {
+    console.log('event')
+    if (slider > 0) {
+      slider--;
+      let multipler = slider;
+      let itemMargin = +$(".feedback__item").css("margin-right").split("px")[0];
+      let pad = +$(document).width() * 0.1;
+      let itemWidth = +$(".feedback__item").css("width").split("px")[0];
+      let shift = (itemMargin + itemWidth) * multipler;
+      console.log(shift);
+      $(".feedback__item").css({
+        "left": String(350 - shift) + "px"
+      })
+    }
+  })
 
+
+  $("#next").on('click', event => {
+    if (slider < 2) {
+      slider++;
+      let multipler = slider;
+      let itemMargin = +$(".feedback__item").css("margin-right").split("px")[0];
+      let itemWidth = +$(".feedback__item").css("width").split("px")[0];
+      let pad = +$(document).width() * 0.1;
+      let shift = (itemMargin + itemWidth) * multipler;
+      console.log(shift);
+      $(".feedback__item").css({
+        "left": String(350 - shift) + "px"
+      })
+    }
+  })
 
 })
